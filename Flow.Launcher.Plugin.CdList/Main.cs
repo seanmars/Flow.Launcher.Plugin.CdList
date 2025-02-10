1248ca50051eb7cb15b3fa6c8b05ff7e42fc3acf
seanmars <mars0222@gmail.com>
2025-02-10 12:10:58 +0800

    fix: Add error handling for the path not existing

--- Flow.Launcher.Plugin.CdList/Main.cs
+++ Flow.Launcher.Plugin.CdList/Main.cs
@@ -1,13 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Threading;
 using System.Threading.Tasks;
-using Flow.Launcher.Plugin;
 
 namespace Flow.Launcher.Plugin.CdList
 {
@@ -16,7 +14,10 @@ namespace Flow.Launcher.Plugin.CdList
     /// </summary>
     public class CdList : IAsyncPlugin, IResultUpdated
     {
+        private const string NotExistMessage = "Path does not exist";
+
         private const string IconFolder = "folder.png";
+        private const string IconFolderFail = "folder_fail.png";
 
         private PluginInitContext _context;
 
@@ -35,34 +36,7 @@ namespace Flow.Launcher.Plugin.CdList
         /// <returns></returns>
         public Task<List<Result>> QueryAsync(Query query, CancellationToken token)
         {
-            var results = new List<Result>();
-
-            if (query.Search.Length == 0)
-            {
-                var drives = GetDrives()
-                    .ConvertAll(d => new Result
-                    {
-                        Title = d,
-                        IcoPath = IconFolder,
-                        Action = Action(d),
-                    });
-
-                results.AddRange(drives);
-            }
-            else
-            {
-                var path = query.Search.Trim();
-
-                var directories = GetDirectories(path)
-                    .ConvertAll(d => new Result
-                    {
-                        Title = d,
-                        IcoPath = IconFolder,
-                        Action = Action(d),
-                    });
-
-                results.AddRange(directories);
-            }
+            var results = GetResults(query.Search);
 
             ResultsUpdated?.Invoke(this, new ResultUpdatedEventArgs
             {
@@ -84,8 +58,59 @@ namespace Flow.Launcher.Plugin.CdList
             return Task.CompletedTask;
         }
 
+        private List<Result> GetResults(string path)
+        {
+            var results = new List<Result>();
+
+            try
+            {
+                if (string.IsNullOrEmpty(path))
+                {
+                    var drives = GetDrives()
+                        .ConvertAll(d => new Result
+                        {
+                            Title = d,
+                            IcoPath = IconFolder,
+                            Action = Action(d),
+                        });
+
+                    results.AddRange(drives);
+                }
+                else
+                {
+                    var directories = GetDirectories(path)
+                        .ConvertAll(d => new Result
+                        {
+                            Title = d,
+                            IcoPath = IconFolder,
+                            Action = Action(d),
+                        });
+
+                    results.AddRange(directories);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+                results.Add(new Result
+                {
+                    Title = NotExistMessage,
+                    IcoPath = IconFolderFail,
+                    Action = Action(path),
+                });
+            }
+
+            return results;
+        }
+
         private List<string> GetDirectories(string path)
         {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new DirectoryNotFoundException();
+            }
+
             var directories = Directory
                 .GetDirectories(path, "*", searchOption: _searchOption)
                 // Filter out hidden directories
@@ -100,9 +125,12 @@ namespace Flow.Launcher.Plugin.CdList
             }
 
             var isDirectory = new DirectoryInfo(path).Attributes.HasFlag(FileAttributes.Directory);
-            directories.Add(isDirectory
-                ? path
-                : "No directories found");
+            if (!isDirectory)
+            {
+                throw new DirectoryNotFoundException();
+            }
+
+            directories.Add(path);
 
             return directories;
         }
@@ -141,6 +169,11 @@ namespace Flow.Launcher.Plugin.CdList
             try
             {
                 path = path.Replace('\\', '/');
+
+                if (!Directory.Exists(path))
+                {
+                    return;
+                }
 
                 Process.Start(new ProcessStartInfo()
                 {
---
+++ RELEASE.md
@@ -0,0 +1,13 @@
+﻿# Release
+
+## 1.2.2
+
+- Add error handling for the path not existing
+
+## 1.1.0 / 1.2.0
+
+- Adjust the project structure to comply with official specifications
+
+## 1.0.0
+
+- Initial release
